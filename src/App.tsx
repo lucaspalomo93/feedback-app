8dc9ed5770df653c28cd77fb5c07a43228291407
Lucas Palomo <lucas.palomo@weplan-latam.com>
2024-12-14 22:24:05 -0300

    added final logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import FeedbackData, { FeedbackItemType } from './data/FeedbackData';
-import FeedbackList from './components/FeedbackList';
-import FeedbackStats from './components/FeedbackStats';
-import FeedbackForm from './components/FeedbackForm';
+import { useState } from "react";
+import "./App.css";
+import FeedbackForm from "./components/FeedbackForm";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import Header from "./components/Header";
+import FeedbackData, { FeedbackItemType } from "./data/FeedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState<FeedbackItemType[]>(FeedbackData);
 
   const deleteFeedback = (id: number) => {
-    if (window.confirm('Are you sure you want to delete?')) {
+    if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((f) => f.id !== id));
     }
   };
 
+  const addFeedback = (feedbackItem: FeedbackItemType) => {
+    setFeedback([...feedback, feedbackItem]);
+  };
+
   return (
     <>
-      <Header text={'hello World'} />
-      <div className='container'>
-        <FeedbackForm />
+      <Header text={"hello World"} />
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
